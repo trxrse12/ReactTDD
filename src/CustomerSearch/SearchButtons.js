b9952532aa592f7f0bba72c8b52695e6bcd0787e
trxrse12 <trxrse@gmail.com>
2020-09-21 19:31:02 +0100

    21/09/2020 H1. 1. the new SearchButtons component under construction, it now has a Next and a Previous buttons, also it has dedicated buttons for the size of the data to be retrieved

--- src/CustomerSearch/SearchButtons.js
+++ src/CustomerSearch/SearchButtons.js
@@ -35,8 +35,42 @@ export const SearchButtons = ({
   const hasPrevious = () => lastRowIds.length > 0;
   const hasNext = () => customers.length === limit;
 
+  const limitParams = useCallback((newLimit) => {
+    return {searchTerm, lastRowIds, limit: newLimit}
+  }, [searchTerm, lastRowIds]);
+
+const toggleButton = (limitSize) => limit===limitSize;
+
   return (
     <div>
+      <ToggleRouterButton
+        id="limit-10"
+        pathname = {pathname}
+        toggled={toggleButton(10)}
+        queryParams={limitParams(10)}>
+          10
+      </ToggleRouterButton>
+      <ToggleRouterButton
+        id="limit-20"
+        pathname = {pathname}
+        toggled={toggleButton(20)}
+        queryParams={limitParams(20)}>
+          20
+      </ToggleRouterButton>
+      <ToggleRouterButton
+        id="limit-50"
+        pathname = {pathname}
+        toggled={toggleButton(50)}
+        queryParams={limitParams(50)}>
+          50
+      </ToggleRouterButton>
+      <ToggleRouterButton
+        id="limit-100"
+        pathname = {pathname}
+        toggled={toggleButton(100)}
+        queryParams={limitParams(100)}>
+          100
+      </ToggleRouterButton>
       <RouterButton
         id={'previous-page'}
         queryParams={previousPageParams()}
@@ -54,4 +88,4 @@ export const SearchButtons = ({
       </RouterButton>
     </div>
   );
-}
+};
--- test/CustomerSearch/SearchButtons.test.js
+++ test/CustomerSearch/SearchButtons.test.js
@@ -23,6 +23,7 @@ describe('Search Customers', () => {
         pathname={'/path'}
         lastRowIds={['123']}
         customers={tenCustomers}
+        searchTerm={'term'}
         {...props}
       >
       </SearchButtons>
@@ -93,10 +94,50 @@ describe('Search Customers', () => {
       });
     });
 
-    it('is disabled if there are fewer records than the paga limit shown', () => {
+    it('is disabled if there are fewer records than the page limit shown', () => {
       renderSearchButtons({customers: []});
       const button = elementMatching(id('next-page'));
       expect(button.props.disabled).toBeTruthy();
     });
+  });
+
+  describe('limit toggle buttons', () => {
+    it('has a button with a label of 10 that is initially toggled', () => {
+      renderSearchButtons();
+      const button = elementMatching(id('limit-10'));
+      expect(button).toBeDefined();
+      expect(button.type).toEqual(ToggleRouterButton);
+      expect(button.props.children).toEqual('10');
+      expect(button.props.pathname).toEqual('/path');
+      expect(button.props.toggled).toEqual(true);
+      expect(button.props.queryParams).toEqual({
+        limit: 10,
+        lastRowIds: ['123'],
+        searchTerm: 'term',
+      })
+    });
+
+    [20, 50, 100].forEach(limitSize => {
+      it(`has button with a label of ${limitSize} that is initially not toggled`, () => {
+        renderSearchButtons();
+        const button = elementMatching(id(`limit-${limitSize}`));
+        expect(button).toBeDefined();
+        expect(button.type).toEqual(ToggleRouterButton);
+        expect(button.props.children).toEqual(`${limitSize}`);
+        expect(button.props.pathname).toEqual('/path');
+        expect(button.props.toggled).toEqual(false);
+        expect(button.props.queryParams).toEqual({
+          limit: limitSize,
+          lastRowIds: ['123'],
+          searchTerm: 'term',
+        })
+      });
+
+      it(`has toggled button with label limit-${limitSize} when limit prop is ${limitSize}`, () => {
+        renderSearchButtons({limit: limitSize});
+        const button = elementMatching(id(`limit-${limitSize}`));
+        expect(button.props.toggled).toEqual(true);
+      });
+    })
   });
 });
