d75190f91cf224521fa4752516ec0ab07412e651
trxrse12 <trxrse@gmail.com>
2020-10-02 14:40:15 +0100

    02/10/2020 H5. 1. new saga added to respond to SET_CUSTOMER_FOR_APPOINTMENT

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {App} from './App';
 import {Provider} from 'react-redux';
 import {configureStore} from "./store";
 
+import { Router, Route } from 'react-router';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
---
+++ src/sagas/app.js
@@ -0,0 +1,10 @@
+import {put} from 'redux-saga/effects';
+import {appHistory} from "../history";
+
+export function* customerAdded({customer}) {
+  appHistory.push('/addAppointment');
+  yield put({
+    type: 'SET_CUSTOMER_FOR_APPOINTMENT',
+    customer
+    })
+}
